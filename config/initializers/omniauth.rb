aca8ccea8ef7b979dcf57d4218f281e3e494caec
Kevin Huang <yellow.khaos@gmail.com>
2018-07-16 02:15:04 +0000

    automatically backed up by learn

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.confit.middleware.use OmniAuth::Build do
+Rails.application.config.middleware.use OmniAuth::Build do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
